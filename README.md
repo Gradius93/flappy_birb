1e68b60cb847e9577a7bf64c3443de5617bbef4a
Sean Gray <sean.myles.gray@gmail.com>
2020-05-04 13:44:10 +0100

    second commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # flappy_birb
+
 Practice with making the flappy bird game
+
+Tech used: HTML5, JavaScript
 
 Isolation practice with coding games
--- src/app.js
+++ src/app.js
@@ -1,10 +1,62 @@
+// Draw images to canvas
 var cvs = document.getElementById('canvas')
 var ctx = cvs.getContext('2d')
 
+// Load
 var imageName = new Image()
 
 imageName.src = 'images/sprite1'
 
 var audioName = new Audio()
 
-audioName.src = ""
+audioName.src = 'audio/audio.png'
+
+
+// pipe
+
+var pipe = []
+
+pipe[0] = {
+  x: cvs.width,
+  y: 0
+}
+
+
+// Draw
+
+ctx.drawImage(imageName, X, Y, width, height)
+
+//on keyDown Event: bY - = 20
+
+function draw() {
+
+  for(let i = 0; i < pipe.length; i++) {
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y)
+    ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y+Const)
+
+    pipe[i].x--
+
+    if( pipe[i].x == cvs.width - 188) {
+      pipe.push(
+        x: cvs.width,
+        y: Math.floor(Math.random() * pN.height) - pN.height
+      )
+    }
+  }
+
+
+  ctx.drawImage(bg, 0, 0)
+
+  ctx.drawImage(pipeNorth, pX,pY)
+  ctx.drawImage(pipeSouth, pX,pY+Const)
+
+  ctx.drawImage(fg, 0, cvs.height - fg.height)
+
+  ctx.drawImage(bird, bX, bY)
+
+
+  bY + = gravity
+  requestAnimationFrame(draw)
+}
+
+draw()
