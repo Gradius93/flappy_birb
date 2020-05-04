d7f78568a978f7e3306783ad25b924056fb4cec7
Sean Gray <sean.myles.gray@gmail.com>
2020-05-04 13:25:01 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var cvs = document.getElementById('canvas')
+var ctx = cvs.getContext('2d')
+
+var imageName = new Image()
+
+imageName.src = 'images/sprite1'
+
+var audioName = new Audio()
+
+audioName.src = ""
